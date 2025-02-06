3caf641cc76e345304e53310cd1859fe6afc3a3a
evan bechtol <evanbechtol@gmail.com>
2025-02-06 13:13:34 -0600

    update 1726

--- 1726 Tuple with Same Product.js
+++ 1726 Tuple with Same Product.js
@@ -9,20 +9,11 @@ var tupleSameProduct = function (nums) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             const currentProduct = nums[i] * nums[j];
-
-            if (!products.has(currentProduct)) {
-                products.set(currentProduct, 1);
-            } else {
-                products.set(currentProduct, products.get(currentProduct) + 1);
-            }
+            const count = products.get(currentProduct) || 0;
+            // Each pair of indices can form 8 different tuples with the same product.
+            result += 8 * count;
+            products.set(currentProduct, count + 1);
         }
-    }
-
-    for (const [key, value] of products) {
-        // combination formula: C(n, 2) = n * (n - 1) / 2
-        const pairsOfEqualProduct = ((value - 1) * value) / 2;
-        // Each pair of indices can form 8 different tuples with the same product.
-        result += 8 * pairsOfEqualProduct;
     }
 
     return result;
