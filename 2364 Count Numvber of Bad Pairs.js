79254280cada99f61b1c69830fb574d2a6332447
evan bechtol <evanbechtol@gmail.com>
2025-02-09 11:39:04 -0600

    create soln 2364

---
+++ 2364 Count Numvber of Bad Pairs.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countBadPairs = function (nums) {
+    let res = 0;
+    let map = new Map();
+    let n = nums.length * (nums.length - 1) / 2;
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i] - i)) {
+            res += map.get(nums[i] - i);
+            map.set(nums[i] - i, map.get(nums[i] - i) + 1);
+        } else {
+            map.set(nums[i] - i, 1);
+        }
+    }
+    // console.log(res);
+    return n - res;
+};
