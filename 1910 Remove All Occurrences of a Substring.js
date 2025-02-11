6fc4dc273409614076fed69c5883a7c6ecca12d3
evan bechtol <evanbechtol@gmail.com>
2025-02-11 09:37:44 -0600

    create soln 3 for 1910

--- 1910 Remove All Occurrences of a Substring.js
+++ 1910 Remove All Occurrences of a Substring.js
@@ -38,3 +38,63 @@ var removeOccurrences = function (s, part) {
 
     return s;
 };
+
+// Approach 3: KMP Algorithm
+/**
+ * @param {string} s
+ * @param {string} part
+ * @return {string}
+ */
+var removeOccurrences = function (s, part) {
+    const lps = buildLPS(part);
+    let i = 0; // index for s
+    let j = 0; // index for part
+    let result = '';
+
+    while (i < s.length) {
+        if (s[i] === part[j]) {
+            i++;
+            j++;
+            if (j === part.length) {
+                // Found a match, reset j and remove the matched part from result
+                result = result.slice(0, result.length - part.length + 1);
+                j = 0;
+            }
+        } else {
+            if (j !== 0) {
+                j = lps[j - 1];
+            } else {
+                result += s[i];
+                i++;
+            }
+        }
+    }
+
+    // Append remaining characters
+    result += s.slice(i);
+
+    return result;
+};
+
+const buildLPS = (pattern) => {
+    const lps = new Array(pattern.length).fill(0);
+    let len = 0;
+    let i = 1;
+
+    while (i < pattern.length) {
+        if (pattern[i] === pattern[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len !== 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+
+    return lps;
+};
