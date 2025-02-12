1601ae7fbdca22a21d9cab49c130a6a43bee3fe7
evan bechtol <evanbechtol@gmail.com>
2025-02-12 11:46:02 -0600

    2342

--- 2342 Max Sum of a Pair With Equal Sum of Digits.js
+++ 2342 Max Sum of a Pair With Equal Sum of Digits.js
@@ -1,15 +1,30 @@
-var maximumSum = function(nums) {
-    let mp = new Array(82).fill(-1);
-    let ans = -1;
-
-    for (let num of nums) {
-        let sumDigits = [...String(num)].reduce((sum, d) => sum + Number(d), 0);
-
-        if (mp[sumDigits] !== -1)
-            ans = Math.max(ans, num + mp[sumDigits]);
-
-        mp[sumDigits] = Math.max(mp[sumDigits], num);
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumSum = function (nums) {
+    if (nums.length < 2) {
+        return -1
     }
 
-    return ans;
+    const digitMapping = new Array(82).fill(0);
+    let result = -1;
+
+    for (let num of nums) {
+        let sum = 0;
+        let temp = num;
+
+        while (temp !== 0) {
+            sum += temp % 10;
+            temp = Math.floor(temp / 10);
+        }
+
+        if (digitMapping[sum] !== 0) {
+            result = Math.max(result, num + digitMapping[sum]);
+        }
+
+        digitMapping[sum] = Math.max(digitMapping[sum], num);
+    }
+
+    return result;
 };
