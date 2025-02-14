c5c15077f842737135a691858a7304e1d4bc2032
evan bechtol <evanbechtol@gmail.com>
2025-02-14 11:40:14 -0600

    1352 soln2

--- 1352 Product of K Last Numbers.js
+++ 1352 Product of K Last Numbers.js
@@ -1,5 +1,6 @@
 
-var ProductOfNumbers = function() {
+// Approach 1: TC: O(n) SC: O(n)
+var ProductOfNumbers = function () {
     this.data = [];
 };
 
@@ -27,3 +28,36 @@ ProductOfNumbers.prototype.getProduct = function(k) {
  * obj.add(num)
  * var param_2 = obj.getProduct(k)
  */
+
+// Approach 2: TC: O(1) SC: O(n)
+var ProductOfNumbers = function () {
+    this.products = [1];
+};
+
+/** 
+ * @param {number} num
+ * @return {void}
+ */
+ProductOfNumbers.prototype.add = function (num) {
+    // reset the products, all previous values are now invalid
+    if (num === 0) {
+        this.products = [1];
+    } else {
+        // add the new product to the end of the array
+        this.products.push( this.products.at(-1) * num);
+    }
+};
+
+/** 
+ * @param {number} k
+ * @return {number}
+ */
+ProductOfNumbers.prototype.getProduct = function (k) {
+    // we can't return the last k, if k is larger than our array
+    if (k > this.products.length - 1) {
+        return 0;
+    }
+
+    // use prefix division to get the product of the last k elements
+    return this.products.at(-1) / this.products.at(-k - 1);
+};
