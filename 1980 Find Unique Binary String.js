1a1352091591209164a00868af2ad110467f024f
evan bechtol <evanbechtol@gmail.com>
2025-02-20 08:34:42 -0600

    1980 update

--- 1980 Find Unique Binary String.js
+++ 1980 Find Unique Binary String.js
@@ -1,4 +1,6 @@
 /**
+ * @description https://leetcode.com/problems/find-unique-binary-string/editorial/?envType=daily-question&envId=2025-02-20
+ * Cantor's diagonal argument: https://en.wikipedia.org/wiki/Cantor%27s_diagonal_argument
  * @param {string[]} nums
  * @return {string}
  */
