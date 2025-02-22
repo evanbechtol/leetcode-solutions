14b1c8ac4ade68b41ef39cfc0acd9d6731e75733
evan bechtol <evanbechtol@gmail.com>
2025-02-21 19:53:08 -0600

    88

--- Top Interview 150/88 Merge Sorted Array.js
+++ Top Interview 150/88 Merge Sorted Array.js
@@ -5,16 +5,16 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
-    let r = nums1.length - 1;
+var merge = function (nums1, m, nums2, n) {
+    let last = m + n - 1;
     let i = m - 1;
     let j = n - 1;
 
     while (j >= 0) {
-        if (j >= 0 && nums1[i] > nums2[j]) {
-            nums1[r--] = nums1[i--];
+        if (nums1[i] > nums2[j]) {
+            nums1[last--] = nums1[i--];
         } else {
-            nums1[r--] = nums2[j--];
+            nums1[last--] = nums2[j--];
         }
     }
 };
