ba973f62f1bf5e9315b73f2e119ec0c9a7378a60
Evan Bechtol <evanbechtol@gmail.com>
2025-02-22 11:27:51 -0600

    1028 comments

--- 1028 Recover a Tree From Preorder Traversal.js
+++ 1028 Recover a Tree From Preorder Traversal.js
@@ -13,26 +13,32 @@
 var recoverFromPreorder = function (traversal) {
     let stack = [];
     let i = 0;
+    const n = traversal.length;
 
-    while (i < traversal.length) {
+    while (i < n) {
         let depth = 0;
-        while (i < traversal.length && traversal[i] === '-') {
+
+        // Count the depth of the current node
+        while (i < n && traversal[i] === '-') {
             depth++;
             i++;
         }
 
         let num = 0;
-        while (i < traversal.length && !isNaN(traversal[i])) {
+        // Get the value of the current node
+        while (i < n && !isNaN(traversal[i])) {
             num = num * 10 + parseInt(traversal[i]);
             i++;
         }
 
         let node = new TreeNode(num);
 
+        // Pop nodes from the stack until the depth is less than the current depth
         while (stack.length > depth) {
             stack.pop();
         }
 
+        // If the stack is not empty, add the current node as the child of the top node
         if (stack.length > 0) {
             if (!stack[stack.length - 1].left) {
                 stack[stack.length - 1].left = node;
@@ -41,6 +47,7 @@ var recoverFromPreorder = function (traversal) {
             }
         }
 
+        // Add the current node to the stack
         stack.push(node);
     }
 
