e5390af7d50e016bbedad91e62758f06ef627773
evan bechtol <evanbechtol@gmail.com>
2026-07-27 08:53:19 -0500

    Refactor validation logic in Valid Parentheses solution for clarity

--- Java/20. Valid Parentheses.java
+++ Java/20. Valid Parentheses.java
@@ -46,12 +46,18 @@ Constraints:
     1 <= s.length <= 104
     s consists of parentheses only '()[]{}'.
  */
+
+/**
+ * Time complexity: O(n). You have to check each character at least once in the worst-case.
+ * Space complexity: O(n). In the worst case, you may have to add each character to the stack.
+ */
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
 
         for (char ch : s.toCharArray()) {
-
+            // It's simpler to push the equivalent closing parentheses, because you can do a direct
+            // comparison to the characters, instead of having specific edge case checking logic for open/close characters.
             if (ch == '(') {
                 stack.push(')');
             } else if (ch == '[') {
@@ -59,12 +65,16 @@ class Solution {
             } else if (ch == '{') {
                 stack.push('}');
             } else {
+                // If we get here, the character that we are looking at is a closing parentheses
+                // If the stack is empty, the string is not valid & we return false
+                // OR if the stack pop returns a character that is not the current character, we return false.
                 if (stack.isEmpty() || stack.pop() != ch) {
                     return false;
                 }
             }
         }
 
+        // If the stack is empty, then the string was valid
         return stack.isEmpty();
     }
 }
